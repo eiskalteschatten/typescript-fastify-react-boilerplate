898c299067b3564f3a781313ab882c460606f394
Alex Seifert <alex@alexseifert.com>
2023-01-17 16:30:44 +0100

    Add more code splitting

--- packages/client/src/modules/AuthModule/index.tsx
+++ packages/client/src/modules/AuthModule/index.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { Routes, Navigate, Route } from 'react-router-dom';
 
 import LoginRegistration from 'layouts/LoginRegistration';
+import PageSuspense from 'components/PageSuspense';
 
-import Login from './pages/Login';
-import Register from './pages/Register';
+const Login = React.lazy(() => import('./pages/Login'));
+const Register = React.lazy(() => import('./pages/Register'));
 
 const AuthModule: React.FC = () => {
-
   return (
     <LoginRegistration>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={
+          <PageSuspense>
+            <Login />
+          </PageSuspense>
+        } />
+
+        <Route path='/register' element={
+          <PageSuspense>
+            <Register />
+          </PageSuspense>
+        } />
+
         <Route path='*' element={<Navigate replace to={'/login'} />} />
       </Routes>
     </LoginRegistration>
