72ea8f0d07ced838e5b1e53aa9cb93c4048f70f9
Alex Seifert <alex@alexseifert.com>
2023-01-17 16:58:23 +0100

    Clean up more

--- packages/client/src/layouts/Main/index.tsx
+++ packages/client/src/layouts/Main/index.tsx
@@ -1,8 +1,5 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 
-import { useAppSelector } from 'store/hooks';
-
-import * as constants from '../../constants';
 import Toolbar from './Toolbar';
 
 import styles from './styles.module.scss';
@@ -11,22 +8,12 @@ interface Props {
   children: React.ReactNode;
 }
 
-const NavSidebar = React.lazy(() => import('./NavSidebar'));
-
 const MainLayout: React.FC<Props> = ({ children }) => {
-  const { windowWidth } = useAppSelector(state => state.ui);
-
   return (
     <div className={styles.layout}>
       <Toolbar />
 
       <div className={styles.columnLayout}>
-        {windowWidth >= constants.window.WINDOW_WIDTHS.lgMin && (
-          <Suspense fallback={<div style={{ width: 85 }} />}>
-            <NavSidebar />
-          </Suspense>
-        )}
-
         {children}
       </div>
     </div>
