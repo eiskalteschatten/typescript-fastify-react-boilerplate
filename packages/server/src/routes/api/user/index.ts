82db5dfda5256b3c191ab0f71b25dd0de45544bc
Alex Seifert <alex@alexseifert.com>
2023-04-10 13:55:20 +0200

    Close a security hole

--- packages/server/src/routes/api/user/index.ts
+++ packages/server/src/routes/api/user/index.ts
@@ -4,6 +4,7 @@ import { UserUpdate } from '@tfrb/shared';
 import User from '~/db/models/User';
 import { HttpError } from '~/lib/errors';
 import UserService from '~/services/UserService';
+import { fastifyPreValidationJwt } from '~/auth/fastify';
 
 export default async (app: FastifyInstance) => {
   type RegistrationRequest = FastifyRequest<{ Body: { registrationData: User } }>;
@@ -28,21 +29,18 @@ export default async (app: FastifyInstance) => {
     });
   });
 
-  type UpdateRequest = FastifyRequest<{
-    Body: { updateData: UserUpdate };
-    Params: { userId: string };
-  }>;
+  type UpdateSelfRequest = FastifyRequest<{ Body: { updateData: UserUpdate } }>;
 
-  app.put('/:userId', async (req: UpdateRequest, reply: FastifyReply) => {
-    const userId = Number(req.params.userId);
+  app.put('/self', fastifyPreValidationJwt, async (req: UpdateSelfRequest, reply: FastifyReply) => {
     const updateData = req.body?.updateData;
+    const { user } = req;
 
     if (!updateData) {
       throw new HttpError('No update data was provided!', 400);
     }
 
     const userService = new UserService();
-    await userService.update(userId, updateData);
+    await userService.update(user.id, updateData);
 
     reply.send({
       user: userService.serializeUser(),
