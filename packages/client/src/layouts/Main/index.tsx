02caeef1484787780052f1946c7d8f3de428e943
Alex Seifert <alex@alexseifert.com>
2023-04-11 17:06:21 +0200

    Start preparing for the off canvas menu

--- packages/client/src/layouts/Main/index.tsx
+++ packages/client/src/layouts/Main/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
+import { useAppSelector } from '@/store/hooks';
 import Toolbar from '@/components/Toolbar';
 import SidebarMain from '@/components/SidebarMain';
+
+import * as constants from '@/constants';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const MainLayout: React.FC<Props> = ({ children }) => {
+  const { windowWidth } = useAppSelector(state => state.ui);
+
   return (
     <div className='flex flex-col w-full h-full'>
       <Toolbar />
 
       <div className='w-full h-full flex flex-row'>
-        <SidebarMain />
+        {windowWidth >= constants.window.WINDOW_WIDTHS.lgMin && (
+          <SidebarMain />
+        )}
 
         <div className='w-full h-full relative overflow-auto py-4'>
           {children}
